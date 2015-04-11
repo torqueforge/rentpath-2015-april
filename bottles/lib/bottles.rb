b3199b409658c291c592e71021b896e3e6fb6f92
TJ Stankus <tjstankus@gmail.com>
2015-04-11 13:17:13 -0400

    Concatenate strings in each case for readability

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,25 +10,13 @@ class Bottles
   def verse(num)
     case num
     when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{num} bottles of beer on the wall, " +
-      "#{num} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{num - 1} bottles of beer on the wall.\n"
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
     end
   end
 end
