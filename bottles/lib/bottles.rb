b9d73c10685a422a28bdc4e5f278450c6ff13bd6
TJ Stankus <tjstankus@gmail.com>
2015-04-11 13:20:49 -0400

    DRY out diffs between case 2 and else

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -13,10 +13,16 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} #{container(num-1)} of beer on the wall.\n"
+    end
+  end
+
+  def container(amount)
+    if amount == 1
+      'bottle'
+    else
+      'bottles'
     end
   end
 end
