fcfc7da4ac39ffbef00adadf8f6f69ceffdacf71
TJ Stankus <tjstankus@gmail.com>
2015-04-11 11:37:21 -0400

    Introduce and use phrase method in case 2

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -9,7 +9,7 @@ class House
     when 1
       "This is the house that Jack built.\n"
     when 2
-      "This is the malt that lay in the house that Jack built.\n"
+      "This is #{phrase}the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
@@ -31,5 +31,9 @@ class House
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
+  end
+
+  def phrase
+    'the malt that lay in '
   end
 end
